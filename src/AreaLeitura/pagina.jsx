9c7524171256195b3f090a48c2c35bb3996c6617
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-12-02 20:30:15 -0300

    Update pagina.jsx

--- src/AreaLeitura/pagina.jsx
+++ src/AreaLeitura/pagina.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef } from "react"; 
 import "./components/css/AreaDaLeitura.css";
 import AreaDaLeitura from "./components/Inicio";
 import BoxLivros from "./components/Categorias";
